f62b95ca97379fea9556e196fe007153d79ec7c6
TyDye001 <tylerparis001@gmail.com>
2023-04-01 02:08:06 -0400

    a

--- StrongMuscleSimulator.lua
+++ StrongMuscleSimulator.lua
@@ -19,7 +19,7 @@ _G.selectEgg = "Basic"
 _G.equipBestPets = true
 _G.teleportPlayer = true
 _G.playerName = "String"
-_G.dfSpawn = "String"
+getgenv().autoEasterEgg = true
 
 
 --Functions
@@ -93,7 +93,7 @@ end
 function autoHatch()
 	while _G.autoHatch == true do
 		game:GetService("ReplicatedStorage").Events.HatchEgg:InvokeServer(_G.selectEgg,1)
-		wait(.0001)
+		wait(.1)
 	 end
 	end
 
@@ -112,6 +112,57 @@ function equipBestPets()
 		wait(10)
 	 end
 	end
+
+local plr = game.Players.LocalPlayer
+function autoEasterEgg()
+	while getgenv().autoEasterEgg == true do
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1340.19995, 10.4506521, 287.550629, 0.992667794, -1.46975516e-08, 0.12087442, 2.19424052e-08, 1, -5.86060018e-08, -0.12087442, 6.08285688e-08, 0.992667794)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1356.22839, 10.4533472, 236.464783, 0.943055868, 8.99328778e-10, 0.33263433, -3.11141002e-09, 1, 6.11754292e-09, -0.33263433, -6.80414702e-09, 0.943055868)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1383.7583, 10.4545555, 214.96051, 0.969829321, -2.9185804e-08, 0.24378489, 2.09650342e-08, 1, 3.63160275e-08, -0.24378489, -3.01093905e-08, 0.969829321)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1370.61255, 10.4555178, 181.150452, 0.972305417, -1.26833308e-08, -0.233713925, 3.57106664e-08, 1, 9.42962401e-08, 0.233713925, -1.00030817e-07, 0.972305417)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1343.57727, 10.4556074, 157.712738, 0.986661613, -2.72522147e-08, 0.1627848, 3.12946469e-08, 1, -2.22687291e-08, -0.1627848, 2.70659921e-08, 0.986661613)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1362.3811, 10.456809, 147.632797, 0.993912697, 5.42224292e-08, -0.110170402, -6.33189714e-08, 1, -7.9069352e-08, 0.110170402, 8.55639115e-08, 0.993912697)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1343.94763, 10.455533, 125.011818, 0.871247232, -6.76764103e-08, 0.490844399, 1.01362133e-07, 1, -4.20399395e-08, -0.490844399, 8.63802185e-08, 0.871247232)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1321.53687, 10.4552727, 139.569275, 0.982351422, 2.44778207e-08, 0.187044606, -1.42775813e-09, 1, -1.23367684e-07, -0.187044606, 1.2092336e-07, 0.982351422)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1300.67883, 10.4544983, 99.9801636, 0.991015077, 7.97494053e-08, 0.13375023, -9.11104863e-08, 1, 7.88219836e-08, -0.13375023, -9.0299821e-08, 0.991015077)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1290.72107, 10.4540491, 135.269333, -0.964441121, 1.93805114e-08, -0.264297724, 2.54764299e-09, 1, 6.40318021e-08, 0.264297724, 6.10815718e-08, -0.964441121)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1274.67688, 10.4524736, 159.181717, -0.979560435, 2.1905846e-08, 0.20115, 2.27973231e-08, 1, 2.11538853e-09, -0.20115, 6.65783251e-09, -0.979560435)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1237.38367, 10.4497175, 189.512787, -0.710348129, -6.97620877e-08, -0.703850448, -1.12982859e-08, 1, -8.77123369e-08, 0.703850448, -5.43539933e-08, -0.710348129)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1232.05371, 10.4484596, 217.914703, -0.989347458, 1.17631522e-08, -0.145573348, 1.24872543e-10, 1, 7.99570117e-08, 0.145573348, 7.90870871e-08, -0.989347458)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1225.14331, 10.4463873, 243.266235, -0.899308205, -1.17401191e-08, -0.437315404, 9.48150181e-09, 1, -4.63439243e-08, 0.437315404, -4.5823878e-08, -0.899308205)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+		plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1261.70227, 10.449338, 230.374176, 0.579262316, 1.02469983e-07, 0.815141201, -4.50554509e-08, 1, -9.36905806e-08, -0.815141201, 1.75448687e-08, 0.579262316)
+		wait(.1)
+		if getgenv().autoEasterEgg == false then break end
+	end
+end
 
 --Tabs
 
@@ -171,6 +222,15 @@ FarmTab:AddToggle({
 	end
 })
 
+FarmTab:AddToggle({
+	Name = "Auto Easter Egg Event",
+	Default = false,
+	Callback = function(Value)
+		getgenv().autoEasterEgg = (Value)
+		autoEasterEgg()
+	end
+})
+
 SkillTab:AddToggle({
 	Name = "Fast Punch",
 	Default = false,
@@ -201,7 +261,7 @@ SkillTab:AddToggle({
 EggTab:AddDropdown({
 	Name = "Select Egg",
 	Default = "Basic",
-	Options = {"Basic", "Town", "Beach", "Crystal", "Forest", "Desert", "Hell", "Cat", "Void", "Anime", "Atlantis", "Angel", "Boss"},
+	Options = {"Basic", "Town", "Beach", "Crystal", "Forest", "Desert", "Hell", "Cat", "Void", "Anime", "Atlantis", "Angel", "Boss", "Easter"},
 	Callback = function(Value)
 		_G.selectEgg = (Value)
 	end    
@@ -239,7 +299,7 @@ EggTab:AddToggle({
 MapTab:AddDropdown({
 	Name = "Select Area",
 	Default = "Spawn",
-	Options = {"Spawn", "Jungle", "Beach", "Space", "Forest", "Desert", "Hell", "Candy", "Void", "Anime", "Moon", "Heaven", "Gym", "Candy Training", "Moon Training", "Boss", "Above The Map"},
+	Options = {"Spawn", "Jungle", "Beach", "Space", "Forest", "Desert", "Hell", "Candy", "Void", "Anime", "Moon", "Heaven", "Gym", "Candy Training", "Moon Training", "Boss", "Easter", "Above The Map"},
 	Callback = function(Value)
 		getgenv().TPPlace = (Value)
 	end    
@@ -305,6 +365,8 @@ MapTab:AddButton({
 		Teleport(game:GetService("Workspace").ScriptSabitleri.Training3.CFrame)
 		elseif getgenv().TPPlace == "Boss" then
 		Teleport(game:GetService("Workspace").BossSpawn.CFrame)
+		elseif getgenv().TPPlace == "Easter" then
+		Teleport(game:GetService("Workspace").EasterSpawn.CFrame)
 		elseif getgenv().TPPlace == "Above The Map" then
 		Teleport(CFrame.new(-1319.40442, 89.5042191, 209.569458, 0.979221344, -1.4935889e-08, 0.202794418, 1.57723434e-08, 1, -2.50858401e-09, -0.202794418, 5.65500224e-09, 0.979221344))
   	end  
